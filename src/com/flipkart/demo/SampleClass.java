9179b8c84e641285ddd9ec4cf242cb1a48053efe
prachiragrawal <prachir.agrawal@flipkart.com>
2024-01-02 13:37:32 +0530

    Initial Java Commit

---
+++ src/com/flipkart/demo/SampleClass.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.flipkart.demo;
+
+/**
+ * 
+ */
+public class SampleClass {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World in Java for Flipkart");
+
+	}
+
+}
